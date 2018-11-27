22f71165f359c141456ef5dde77e071dcfc8bb33
Alkhasar <31177177+Alkhasar@users.noreply.github.com>
2018-11-27 21:32:06 +0100

    Commit Iniziale

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// Includes here
+#include <iostream>
+
+// Namespace used
+using namespace std;
+
+// Main function that will be launched by compiler
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
